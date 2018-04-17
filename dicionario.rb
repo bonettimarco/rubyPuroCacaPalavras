52a7fd8751f9d85540dff2d2f139c98857f5e226
Joao Bernardo <joaobernardo@id.uff.br>
2018-04-17 16:35:36 -0300

    gerando todas as matrizes

--- dicionario.rb
+++ dicionario.rb
@@ -4,7 +4,7 @@ class Dicionario
   attr_reader :coordenadas, :entrada, :matriz, :coordenadas_esquerda_direita, :coordenadas_direita_esquerda
 
   def initialize
-	  @entrada = ["Lao", "New", "San", "Sao", "Sri", "Cape", "Chad", "Cook", "Cuba", "East", "Fiji", "Guam", "Hong", "Iran", "Iraq", "Mali", "Niue", "Oman", "Peru", "Togo", "Aruba", "Benin", "Chile", "China", "Cocos", "Congo", "Costa", "Czech", "Egypt", "Faroe", "Gabon", "Ghana", "Haiti", "Heard", "India", "Italy", "Ivory", "Japan", "Kenya", "Korea", "Macau", "Malta", "Nauru", "Nepal", "Niger", "Palau", "Papua", "Qatar", "Saint", "Samoa", "Saudi", "South", "Spain", "S udan", "Tonga", "Turks", "Yemen", "Zaire", "Angola", "Belize", "Bhutan", "Bosnia", "Bouvet", "Brazil", "Brunei", "Canada", "Cayman", "Cyprus", "France", "French", "Gambia", "Greece", "Guinea", "Guyana", "Israel", "Jordan", "Kuwait", "Latvia", "Libyan", "Malawi", "Mexico", "Monaco", "Norway", "Panama", "Poland", "Puerto", "Rwanda", "Sierra", "Sweden", "Syrian", "Taiwan", "Turkey", "Tuvalu", "Uganda", "United", "Virgin", "Wallis", "Zambia", "Albania", "Algeria", "Andorra", "Antigua", "Armenia&quo t", "Austria", "Bahamas", "Bahrain", "Belarus", "Belgium", "Bermuda", "Bolivia", "British", "Burkina", "Burundi", "Central", "Comoros", "Croatia", "Denmark", "Ecudaor", "Eritrea", "Estonia", "Finland", "Georgia", "Germany", "Grenada", "Hungary", "Iceland", "Ireland", "Jamaica", "Lebanon", "Lesotho", "Liberia", "Mayotte", "Moldova", "Morocco", "Myanmar", "Namibia", "Nigeria", "Norfork", "Reunion", "Romania", "Russian", "Senegal", "Solomon", "Somalia", "Tokelau", "Tunisia", "Ukraine", "Uruguay", "Vanuatu", "Vatican" , "Vietnam", "Virigan", "Western", "American", "Anguilla", "Barbados", "Botswana", "Bulgaria", "Cambodia", "Cameroon", "Colombia", "Djibouti", "Dominica", "Ethiopia", "Falkland", "Honduras", "Kiribati", "Malaysia", "Maldives", "Marshall", "Mongolia", "Northern", "Pakistan", "Paraguay", "Pitcairn", "Portugal", "Slovakia", "Slovenia", "Suriname", "Svalbarn", "Tanzania", "Thailand", "Trinidad", "Zimbabwe", "Argentina", "Australia", "Christmas", "Dominican", "Gibraltar", "Greenland", "Guatemala", "Indonesia", "Lithuania", "Macedonia", "Maurit ius", "Nicaragua", "Singapore", "Swaziland", "Venezuela", "Antarctica", "Azerbaijan", "Bangladesh", "Equatorial", "Guadeloupe", "Kazakhstan", "Kyrgyzstan", "Luxembourg", "Madagascar", "Martinique", "Mauritania", "Micronesia", "Montserrat", "Mozambique", "Seychelles", "Tajikistan", "Uzbekistan", "Yugoslavia", "Afghanistan", "Netherlands", "Philippines", "Switzerland", "Turkmenistan", "Liechtenstein"];
+	  @entrada = ["Lao", "New", "San", "Sao", "Sri", "Cape", "Chad", "Cook", "Cuba", "East", "Fiji", "Guam", "Hong", "Iran", "Iraq", "Mali", "Niue", "Oman", "Peru", "Togo", "Aruba", "Benin", "Chile", "China", "Cocos", "Congo", "Costa", "Czech", "Egypt", "Faroe", "Gabon", "Ghana", "Haiti", "Heard", "India", "Italy", "Ivory", "Japan", "Kenya", "Korea", "Macau", "Malta", "Nauru", "Nepal", "Niger", "Palau", "Papua", "Qatar", "Saint", "Samoa", "Saudi", "South", "Spain", "Sudan", "Tonga", "Turks", "Yemen", "Zaire", "Angola", "Belize", "Bhutan", "Bosnia", "Bouvet", "Brasil", "Brunei", "Canada", "Cayman", "Cyprus", "France", "French", "Gambia", "Greece", "Guinea", "Guyana", "Israel", "Jordan", "Kuwait", "Latvia", "Libyan", "Malawi", "Mexico", "Monaco", "Norway", "Panama", "Poland", "Puerto", "Rwanda", "Sierra", "Sweden", "Syrian", "Taiwan", "Turkey", "Tuvalu", "Uganda", "United", "Virgin", "Wallis", "Zambia", "Albania", "Algeria", "Andorra", "Antigua", "Armenia&quo t", "Austria", "Bahamas", "Bahrain", "Belarus", "Belgium", "Bermuda", "Bolivia", "British", "Burkina", "Burundi", "Central", "Comoros", "Croatia", "Denmark", "Ecudaor", "Eritrea", "Estonia", "Finland", "Georgia", "Germany", "Grenada", "Hungary", "Iceland", "Ireland", "Jamaica", "Lebanon", "Lesotho", "Liberia", "Mayotte", "Moldova", "Morocco", "Myanmar", "Namibia", "Nigeria", "Norfork", "Reunion", "Romania", "Russian", "Senegal", "Solomon", "Somalia", "Tokelau", "Tunisia", "Ukraine", "Uruguay", "Vanuatu", "Vatican" , "Vietnam", "Virigan", "Western", "American", "Anguilla", "Barbados", "Botswana", "Bulgaria", "Cambodia", "Cameroon", "Colombia", "Djibouti", "Dominica", "Ethiopia", "Falkland", "Honduras", "Kiribati", "Malaysia", "Maldives", "Marshall", "Mongolia", "Northern", "Pakistan", "Paraguay", "Pitcairn", "Portugal", "Slovakia", "Slovenia", "Suriname", "Svalbarn", "Tanzania", "Thailand", "Trinidad", "Zimbabwe", "Argentina", "Australia", "Christmas", "Dominican", "Gibraltar", "Greenland", "Guatemala", "Indonesia", "Lithuania", "Macedonia", "Maurit ius", "Nicaragua", "Singapore", "Swaziland", "Venezuela", "Antarctica", "Azerbaijan", "Bangladesh", "Equatorial", "Guadeloupe", "Kazakhstan", "Kyrgyzstan", "Luxembourg", "Madagascar", "Martinique", "Mauritania", "Micronesia", "Montserrat", "Mozambique", "Seychelles", "Tajikistan", "Uzbekistan", "Yugoslavia", "Afghanistan", "Netherlands", "Philippines", "Switzerland", "Turkmenistan", "Liechtenstein"];
 		@matriz = Matriz.new
  		@coordenadas=""
   end
@@ -15,10 +15,11 @@ class Dicionario
 		end
   end
 
-  def encontra_palavra(tipo, matriz)
+
+  def encontra_palavra(tipo, matriz, tamanho, colunas)
   	valor = 0
   	achou = 0
-  	sentido = ["horizontal, esquerda para a direita","vertical de cima para baixo","horizontal, direita para a esquerda","vertical de baixo para cima","diagonal, cima para baixo, esquerda para a direita","diagonal, cima para baixo, esquerda para a direita","diagonal, baixo para cima, esquerda para a direita","diagonal, baixo para cima, esquerda para a direita"]
+  	sentido = ["horizontal, esquerda para a direita","vertical de cima para baixo","horizontal, direita para a esquerda","vertical de baixo para cima","diagonal, cima para baixo, esquerda para a direita","diagonal, cima para baixo, esquerda para a direita","diagonal, baixo para cima, esquerda para a direita","diagonal, baixo para cima, direita para a esquerda"]
   	entrada.each do |e|
   		regex = Regexp.new(e.upcase)
   			matriz.each_with_index do |r,index|
@@ -42,9 +43,32 @@ class Dicionario
 	  				 num_linha = linha.size-(regex =~ linha)
 	  				 num_coluna = index+1
 	  				elsif tipo=='d_b_c_e_d'
-	  					valor = 5
-	  				 num_linha = index+1
-	  				 num_coluna = (regex =~ linha)+1
+	  							valor = 7
+			  				 	if (index < tamanho)
+				  				 	num_linha = index+1
+					  				num_coluna = (regex =~ linha)+1
+			  				 	elsif index+1 < colunas 
+			  				 		num_linha = (index-tamanho-(regex =~ linha)-1)  
+					  				num_coluna = index+((regex =~ linha))-1-num_linha
+			  				 	else
+			  				 		num_linha = tamanho-(regex =~ linha) 
+					  				num_coluna = index+((regex =~ linha))-1-num_linha
+			  				 end
+	  				elsif tipo=='d_b_c_d_e'
+	  					valor = 6
+							p index
+							p colunas
+							p tamanho
+	  				 	if (index < tamanho)
+		  				 	num_linha = index+1
+			  				num_coluna = (regex =~ linha)+1
+	  				 	elsif index+1 < colunas 
+	  				 		num_linha = (index-tamanho-(regex =~ linha)-1)  
+			  				num_coluna = index+((regex =~ linha))-1-num_linha
+	  				 	else
+	  				 		num_linha = tamanho-(regex =~ linha) 
+			  				num_coluna = index+((regex =~ linha))-1-num_linha
+	  				 end
 	  				end
 	    			coordenadas << "| -- |No sentido "+sentido[valor]+" achou a cidade "+e+" na linha "+num_linha.to_s+" e na coluna "+num_coluna.to_s+"\n"
 	    		end
--- matriz.rb
+++ matriz.rb
@@ -7,15 +7,14 @@ class Matriz
   	@esquerda_direita = Array.new
 	  monta_matriz_base 
 	  @vertical_cima_baixo = @esquerda_direita.transpose
-#	  @direita_esquerda = @esquerda_direita.reverse.transpose
-#	  @vertical_baixo_cima = @direita_esquerda.reverse.transpose.reverse
 	  @vertical_baixo_cima = @esquerda_direita.reverse.transpose
 	  @direita_esquerda = @vertical_baixo_cima.reverse.transpose.reverse
 	  @diagonal_ascendente_esquerda_direita = Array.new
 	  @diagonal_ascendente_direita_esquerda = Array.new
-	  cria_diagonal
+	  cria_diagonal(@esquerda_direita, @diagonal_ascendente_esquerda_direita, @diagonal_ascendente_direita_esquerda)
 	  @diagonal_descendente_esquerda_direita = Array.new
 	  @diagonal_descendente_direita_esquerda = Array.new
+	  cria_diagonal(@direita_esquerda, @diagonal_descendente_esquerda_direita, @diagonal_descendente_direita_esquerda)
  	  @todas_as_matrizes = [@esquerda_direita, @vertical_cima_baixo, @direita_esquerda, @vertical_baixo_cima]
 	end
 
@@ -24,14 +23,23 @@ class Matriz
   	esquerda_direita << ['G','O','R','K','S','O','R','H','R','H','D','D','I','R','J','L','A','D','N','E','M','I','R','H','S','R','A','O','F']
   	esquerda_direita << ['C','O','R','I','L','A','O','I','S','N','G','H','D','I','R','O','S','P','N','P','E','O','R','S','K','R','O','S','P']
   	esquerda_direita << ['A','H','R','I','D','J','C','I','A','O','G','H','D','I','R','O','S','E','I','A','J','R','O','S','K','D','N','F','O']
-  	esquerda_direita << ['C','O','I','I','D','J','U','L','S','O','G','H','D','I','R','O','S','I','E','H','L','O','S','K','R','N','O','S','I']
-  	esquerda_direita << ['C','O','R','N','D','J','R','I','S','O','G','H','D','I','R','O','S','O','W','I','E','R','R','O','S','I','R','J','O']
-  	esquerda_direita << ['C','O','R','I','A','E','A','I','S','O','G','H','D','I','R','O','S','O','S','I','D','I','E','J','F','O','S','I','I']
-  	esquerda_direita << ['N','E','W','J','Z','I','F','O','S','H','D','R','H','B','E','I','S','O','R','A','B','U','C','Z','E','O','R','I','S']
-  	esquerda_direita << ['C','O','R','T','D','J','R','I','G','O','G','H','D','I','R','O','S','S','U','J','O','D','F','J','T','S','O','F','O']
-  	esquerda_direita << ['C','O','I','I','D','J','R','I','S','N','G','H','D','I','R','O','S','D','R','I','F','O','S','D','D','I','F','O','O']
-  	esquerda_direita << ['C','W','R','I','D','J','R','I','S','E','P','A','C','I','R','O','S','S','E','E','R','I','J','W','O','E','W','S','O']
+  	esquerda_direita << ['C','O','I','I','D','J','U','L','S','O','G','H','D','A','R','O','S','I','E','H','L','O','S','K','R','N','O','S','I']
+  	esquerda_direita << ['C','O','R','N','D','J','R','I','S','O','G','H','D','L','R','O','S','O','W','I','E','R','R','O','S','I','R','J','O']
+  	esquerda_direita << ['C','O','R','I','A','E','A','I','S','O','G','A','D','I','I','O','S','O','S','I','D','I','E','J','F','O','S','I','I']
+  	esquerda_direita << ['N','E','W','J','Z','I','F','O','S','H','N','R','H','B','E','S','S','O','R','A','B','U','C','Z','E','O','R','I','S']
+  	esquerda_direita << ['C','O','R','T','D','J','R','I','G','A','G','H','D','I','R','O','A','S','U','J','O','D','F','J','T','S','O','F','O']
+  	esquerda_direita << ['C','O','I','I','D','J','R','I','C','N','G','H','D','I','R','O','S','R','R','I','F','O','S','D','D','I','F','O','O']
+  	esquerda_direita << ['C','W','R','I','D','J','R','I','S','E','P','A','C','I','R','O','S','S','B','E','R','I','J','W','O','E','W','S','O']
   	esquerda_direita << ['S','L','I','E','C','H','T','E','N','S','T','E','I','N','N','O','S','J','P','E','J','P','Q','E','W','P','Q','S','O']
+  end
+
+  def matriz_referência
+  	(0..12).each do |linha|
+  		(0..29).each do |coluna|
+  		end
+  	end
+
+  	
   end
 
 
@@ -41,11 +49,11 @@ def invertecria_diagonal_b
 	
 end
 
-  def cria_diagonal
+  def cria_diagonal(matriz, matriz_ed, matriz_de)
  	
-  	linhas_original = esquerda_direita.size
-  	linhas = esquerda_direita.size+esquerda_direita[0].size-1
-  	colunas = esquerda_direita[0].size
+  	linhas_original = matriz.size
+  	linhas = matriz.size+matriz[0].size-1
+  	colunas = matriz[0].size
   	i = 0
   	j = 0
   	l = 0
@@ -60,12 +68,12 @@ end
 			end
 			(j..colunas-1).each do |indice|
 							if linha_regressiva >= 0 && indice+coluna_crescente < colunas
-								auxiliar << esquerda_direita[linha_regressiva][indice+coluna_crescente] 
+								auxiliar << matriz[linha_regressiva][indice+coluna_crescente] 
 					  		linha_regressiva = linha_regressiva -1;
 					  	end
 				end
-				diagonal_ascendente_esquerda_direita << auxiliar.split("")
-				diagonal_ascendente_direita_esquerda << auxiliar.reverse.split("")
+				matriz_ed << auxiliar.split("")
+				matriz_de << auxiliar.reverse.split("")
 				auxiliar.clear
 			if l < linhas_original-1
 				i = i + 1
